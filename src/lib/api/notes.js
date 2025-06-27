d7278cff56bde7a009ebd331614274e2c0f6a6eb
Nidhibhide <nidhibhide144@gmail.com>
2025-06-27 11:28:49 +0530

    fix:remove BASE_URL from .env

--- src/lib/api/notes.js
+++ src/lib/api/notes.js
@@ -1,14 +1,15 @@
+
 import axios from "axios";
 
-//api calls
+// Create note
 export const create = async (data) => {
   const response = await axios.post("/api/notes", data);
   return response.data;
 };
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+// Get all notes
 export const getAll = async () => {
-  const response = await fetch(`${BASE_URL}/api/notes`, {
+  const response = await fetch("/api/notes", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -21,14 +22,19 @@ export const getAll = async () => {
   return response.json();
 };
 
+// Delete by ID
 export const deleteById = async (id) => {
   const response = await axios.delete(`/api/notes/${id}`);
   return response.data;
 };
+
+// Get by ID
 export const getById = async (id) => {
   const response = await axios.get(`/api/notes/${id}`);
   return response.data;
 };
+
+// Edit note
 export const edit = async (id, data) => {
   const response = await axios.put(`/api/notes/${id}`, data);
   return response.data;
